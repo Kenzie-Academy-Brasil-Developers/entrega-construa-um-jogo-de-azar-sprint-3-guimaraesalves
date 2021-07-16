c5bb830712664e17de3f1c7132fb79639121a1a4
Mateus Alves <guimaraesalves45@gmail.com>
2021-07-16 14:36:57 -0300

    commit acerto entrega 2

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function jogar(jogador) {
     jogadorescolha = jogador
 
     if (jogadorescolha == 1) {
-        alert("Você escolheu PEDRA.")
+        document.getElementById('usuario').innerHTML = "VOCÊ ESCOLHEU PEDRA!"
     } else if (jogadorescolha == 2) {
-        alert ("Você escolheu TESOURA.")
+        document.getElementById('usuario').innerHTML = "VOCÊ ESCOLHEU PAPEL!"
     } else if (jogadorescolha == 3) {
-        alert("Você escolheu PAPEL. ")
+        document.getElementById('usuario').innerHTML = "VOCÊ ESCOLHEU TESOURA!"
     }
     
 
@@ -18,23 +18,23 @@ function jogar(jogador) {
     //alert(resp)
     
     if ((resp == 1) && ( jogadorescolha == 1)){
-        return document.getElementById('result').innerHTML = "EU TAMBÉM ESCOLHI PEDRA! EMPATAMOS!"
+        return document.getElementById('result').innerHTML = "EU TAMBÉM ESCOLHI PEDRA! EMPATAMOS!👊"
     } else if ((resp == 1) && (jogadorescolha == 2)){
-        return document.getElementById('result').innerHTML = "EU ESCOLHI PEDRA! EU GANHEI!"
+        return document.getElementById('result').innerHTML = "EU ESCOLHI PEDRA! VOCÊ TESOURA! VOCÊ GANHOU!👏"
     } else if ((resp == 1) && (jogadorescolha == 3)){
-        return document.getElementById('result').innerHTML = "EU ESCOLHI PEDRA! VOCÊ GANHOU!"
+        return document.getElementById('result').innerHTML = "EU ESCOLHI PEDRA! VOCÊ ESCOLHEU TESOURA! EU GANHEI!👋"
     } else if ((resp == 2) && (jogadorescolha == 1)){
-        return document.getElementById('result').innerHTML = "EU ESCOLHI TESOURA! VOCÊ GANHOU!"
+        return document.getElementById('result').innerHTML = "EU ESCOLHI PAPEL! VOCÊ ESCOLHEU PEDRA! VOCÊ GANHOU!👏"
     } else if ((resp == 2) && (jogadorescolha == 2)){
-        return document.getElementById('result').innerHTML = "EU TAMBÉM ESCOLHI TESOURA! EMPATAMOS!"
+        return document.getElementById('result').innerHTML = "EU TAMBÉM ESCOLHI PAPEL! VOCÊ TAMBÉM! EMPATAMOS!👊"
     } else if ((resp == 2) && (jogadorescolha == 3)) {
-        return document.getElementById('result').innerHTML = "EU ESCOLHI TESOURA! EU GANHEI!"
+        return document.getElementById('result').innerHTML = "EU ESCOLHI PAPEL! TESOURA!  EU GANHEI!👋"
     } else if ((resp == 3) && (jogadorescolha == 1)) {
-        return document.getElementById('result').innerHTML = "EU ESCOLHI PAPEL! EU GANHEI!"
+        return document.getElementById('result').innerHTML = "EU ESCOLHI TESOURA! VOCÊ PEDRA! VOCÊ GANHOU!👏"
     } else if ((resp == 3) && (jogadorescolha == 2)) {
-        return document.getElementById('result').innerHTML = "EU ESCOLHI PAPEL! VOCÊ GANHOU!"
+        return document.getElementById('result').innerHTML = "EU ESCOLHI TESOURA!  VOCÊ PAPEL! GANHEI!👋"
     } else if ((resp == 3) && (jogadorescolha == 3)) {
-        return document.getElementById('result').innerHTML = "EU TAMBÉM ESCOLHI PAPEL! EMPATAMOS!"
+        return document.getElementById('result').innerHTML = "EU TAMBÉM ESCOLHI TESOURA! EMPATAMOS!👊"
     }
 
     
